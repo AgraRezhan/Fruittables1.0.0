77be2de70a74070289e75ca258e5abc34f5c1ee0
ismail <ismailbary2@gmail.com>
2024-06-14 19:34:35 +0800

    feature: add login register and middlewares

---
+++ Back-End/src/controllers/auth.controller.js
@@ -0,0 +1,77 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { user: UserModel } = require("../models");
+
+const register = async(req, res, next) => {
+    const { nama, email, password } = req.body;
+
+    try {
+        //pengecekan email
+        const userExist = await UserModel.findOne({ where: { email } });
+        if (userExist) {
+            return res.status(401).json({ message: "Email already exist" });
+        } else {
+            //create new user
+            const passwordHash = await bcrypt.hash(password, 10);
+
+            const user = await UserModel.create({
+                nama,
+                email,
+                password: passwordHash,
+            });
+
+
+            if (!user) {
+                return res.status(500).send({
+                    message: "Failed to register user",
+                    data: null,
+                });
+            }
+
+            return res.send({
+                message: "User successfully registered",
+                data: null,
+            });
+        }
+    } catch (err) {
+        next(err);
+    }
+};
+
+const login = async(req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await UserModel.findOne({ where: { email } });
+
+        if (!user) {
+            return res.status(401).json({ message: "Invalid email/password" });
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+
+        if (!isValid) {
+            return res.status(401).json({ message: "Invalid email/password" });
+        }
+
+        const data = {
+            id: user.id,
+            email: user.email,
+        };
+        const token = jwt.sign(data, process.env.JWT_SECRET);
+
+        return res.send({
+            message: "Login successful",
+            data: {
+                token: token,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = {
+    login,
+    register,
+};
--- Back-End/src/index.js
+++ Back-End/src/index.js
@@ -6,8 +6,19 @@ const express = require("express");
 
 const app = express();
 
+const authRouter = require("./routes/auth.router")
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (!process.env.JWT_SECRET) {
+    console.error(
+        "JWT_SECRET is not provided, fill it with random string or generate it using 'openssl rand -base64/-hex 32'"
+    );
+    process.exit(1);
+}
+
+app.use("/api/auth", authRouter)
 
 app.listen(process.env.SERVER_PORT || 3000, () => {
     console.log("Server Running");
