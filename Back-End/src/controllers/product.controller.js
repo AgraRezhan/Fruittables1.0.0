22d57bcc7b3ddbf53513b0f964eb3ad60a956171
ismail <ismailbary2@gmail.com>
2024-06-23 01:27:06 +0800

    feature: add create product in role

--- Back-End/src/controllers/product.controller.js
+++ Back-End/src/controllers/product.controller.js
@@ -23,7 +23,7 @@ const index = async(req, res, _next) => {
                 },
             });
         } else {
-            return res.status(403).send({ message: "Forbidden: Invalid role" });
+            return res.status(403).send({ message: "role tidak valid" });
         }
 
         return res.send({
@@ -36,4 +36,45 @@ const index = async(req, res, _next) => {
     }
 };
 
-module.exports = { index };
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} _next
+ */
+
+const create = async(req, res, _next) => {
+    try {
+        const currentUser = req.user;
+
+        if (currentUser.role !== 'seller') {
+            return res.status(403).send({ message: "hanya seller yang dapat menambahkan produk" });
+        }
+
+        const { title, description, price, stock, img_url } = req.body;
+
+        // Validasi input
+        if (!title || !description || !price || !stock || !img_url) {
+            return res.status(400).send({ message: "permintaaan tidak valid" });
+        }
+
+        const newProduct = await ProductModel.create({
+            user_id: currentUser.id,
+            title,
+            description,
+            price,
+            stock,
+            img_url,
+        });
+
+        return res.send({
+            message: "Product created successfully",
+            data: newProduct,
+        });
+    } catch (error) {
+        console.error("Error:", error);
+        return res.status(500).send({ message: "Internal Server Error" });
+    }
+};
+
+module.exports = { index, create };
--- Back-End/src/routes/product.router.js
+++ Back-End/src/routes/product.router.js
@@ -3,10 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const { validateToken } = require("../middlewares/auth")
-const { index } = require("../controllers/product.controller")
+const { index, create } = require("../controllers/product.controller")
 
 // /api/babs
 router.get("/", validateToken, index);
+router.post("/", validateToken, create);
 
 
 module.exports = router;
